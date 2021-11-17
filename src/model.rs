a796dfc1f6a19f0c021d86bc19ad29617525fa05
J/A <archer884@zoho.com>
2021-11-17 17:29:38 -0600

    Correct def schema

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 
 // Of course, there are a boatload of properties here that I have no clue about.
@@ -11,8 +13,15 @@ pub struct Definition {
     pub attribution_text: Option<String>,
     pub source_dictionary: Option<String>,
     pub text: Option<String>,
-    pub labels: Vec<String>,
-    pub citations: Vec<String>,
+
+    // FIXME: same comment as with citations. >.< I mean, I have no idea what labels exist.
+    pub labels: Vec<HashMap<String, String>>,
+
+    // FIXME: I have no freaking clue why this isn't just a list of strings, but apparently this
+    // property is implemented as a list of maps. Maps of what? No idea. Citations are rare enough
+    // that I don't have a lot to go on, and the docs are unclear.
+    pub citations: Vec<HashMap<String, String>>,
+
     pub word: String,
     #[serde(default = "Vec::new")]
     pub related_words: Vec<String>,
